8315b74ed549fd390950366b6095fce4cef3279d
amm1700 <amm1700@humboldt.edu>
2018-02-05 17:22:01 -0800

    Add files via upload

---
+++ ObsCovs_forloop.R
@@ -0,0 +1,37 @@
+##for loop that arranges phib data into occupancy data Dan & Alyssa 12/14/2017
+
+setwd("C:/Users/db1876/Google Drive/alyssa/")
+
+phibs <- read.csv("Amphib.csv") #read in data
+phibs<-phibs[which(phibs$VES == 0 & phibs$Survey==1|phibs$Survey==2),]
+
+
+sites <- unique(phibs$Site.Code) #get list of sites
+#length(unique(phibs$SiteCode))
+
+occdata <- data.frame(site=sites,Obs1=rep(NA,length(sites)),Obs2=rep(NA,length(sites)),Obs3=rep(NA,length(sites)),Obs4=rep(NA,length(sites)),
+                      Tmax1=rep(NA,length(sites)),Tmax2=rep(NA,length(sites)),Tmax3=rep(NA,length(sites)),Tmax4=rep(NA,length(sites)),MinofDay1=rep(NA,length(sites)),
+                      MinofDay2=rep(NA,length(sites)),MinofDay3=rep(NA,length(sites)),MinofDay4=rep(NA,length(sites))) #make empty data frame WITH FOUR COLUMNS NOW
+
+
+for(i in 1:length(sites)) {
+  
+  site <- sites[i] ##get name of current site
+  
+  curdata <- phibs[which(phibs$Site.Code==site & phibs$Year==2016),] ##get data from current site in 2016
+  
+  if(curdata$Survey==1){ occdata$Obs1[i] <- curdata$Observer }
+  if(curdata$Survey==2){ occdata$Obs2[i] <- curdata$Observer }
+}
+
+  curdata <- phibs[which(phibs$Site.Code==site & phibs$Year==2017),] ##get data from current site in 2017
+
+  if(curdata$Survey==1){ occdata$Obs3[i] <- curdata$Observer }
+  if(curdata$Survey==2){ occdata$Obs4[i] <- curdata$Observer }
+}
+
+
+
+table(occdata$y.1,occdata$y.2) ##quick look at data
+write.csv(occdata,file="Occdata_ASTR_MNSTM.csv")
+
