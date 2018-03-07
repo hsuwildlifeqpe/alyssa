5e09cb62b7dbdcb4e0419f6951dc3f9e03465092
amm1700 <amm1700@humboldt.edu>
2018-03-07 12:50:47 -0800

    Add files via upload

--- ObsCovs_forloop.R
+++ ObsCovs_forloop.R
@@ -1,6 +1,3 @@
-##for loop that arranges phib data into occupancy data Dan & Alyssa 12/14/2017
-
-setwd("C:/Users/db1876/Google Drive/alyssa/")
 
 phibs <- read.csv("Amphib.csv") #read in data
 phibs<-phibs[which(phibs$VES == 0 & phibs$Survey==1|phibs$Survey==2),]
@@ -20,14 +17,14 @@ for(i in 1:length(sites)) {
   
   curdata <- phibs[which(phibs$Site.Code==site & phibs$Year==2016),] ##get data from current site in 2016
   
-  if(curdata$Survey==1){ occdata$Obs1[i] <- curdata$Observer }
-  if(curdata$Survey==2){ occdata$Obs2[i] <- curdata$Observer }
-}
+  if(curdata$Survey==1){ occdata$Obs1[i] <- max(curdata$Observer.1) }
+  if(curdata$Survey==2){ occdata$Obs2[i] <- max(curdata$Observer.1) }
+
 
   curdata <- phibs[which(phibs$Site.Code==site & phibs$Year==2017),] ##get data from current site in 2017
 
-  if(curdata$Survey==1){ occdata$Obs3[i] <- curdata$Observer }
-  if(curdata$Survey==2){ occdata$Obs4[i] <- curdata$Observer }
+  if(curdata$Survey==1) { occdata$Obs3[i] <- max(curdata$Observer.1) }
+  if(curdata$Survey==2) { occdata$Obs4[i] <- max(curdata$Observer.1) }
 }
 
 
